69306d9b085e0f0b9348e520ad53a16237ec8813
HDG66 <101840945+HDG66@users.noreply.github.com>
2025-02-20 22:37:38 -0300

    Add Personal Page files via upload

---
+++ personalPage.js
@@ -0,0 +1,10 @@
+let welcomeEl = document.getElementById("welcome-el");
+
+let name2 = "HDG";
+let greeting2 = "Welcome back: ";
+
+let myGreeting2 = greeting2 + name2;
+
+welcomeEl.textContent = myGreeting2;
+
+welcomeEl.textContent += "👋";
